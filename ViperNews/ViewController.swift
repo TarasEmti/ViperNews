98c2f87e2f52974187df930b3717fe0410acc718
Taras <elouseman@gmail.com>
2020-07-02 11:49:37 +0300

    Initial Commit

---
+++ ViperNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperNews
+//
+//  Created by Тарас Минин on 02/07/2020.
+//  Copyright © 2020 Тарас Минин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
